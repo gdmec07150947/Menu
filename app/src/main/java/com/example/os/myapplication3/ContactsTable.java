dffbe4d7cd5d56727ee2e14efb8fb0d1991a8466
unknown <>
2016-10-30 22:42:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/os/myapplication3/ContactsTable.java
@@ -0,0 +1,33 @@
+package com.example.os.myapplication3;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+public class ContactsTable {
+    private final static String TABLENAME = "contactsTable";
+    private MyDB db;
+    public ContactsTable(Context context)
+    {
+        db = new MyDB(context);
+        if(!db.isTableExits(TABLENAME))
+        {
+            String createTableSql = "CREATE TABLE IF NOT EXISTS " + TABLENAME +"(id_DB integer "+"primary key AUTOCREMENT,"+
+                    User.NAME +" VARCHAR,"+
+                    User.MOBILE +" VARCHAR,"+
+                    User.QQ +" VARCHAR,"+
+                    User.DANWEI +" VARCHAR,"+
+                    User.ADDRESS +" VARCHAR,";
+            db.createTable(createTableSql);
+        }
+    }
+    public boolean addData(User user)
+    {
+        ContentValues values = new ContentValues();
+        values.put(User.NAME,user.getName());
+        values.put(User.MOBILE,user.getMobile());
+        values.put(User.DANWEI,user.getDanwei());
+        values.put(User.QQ,user.getQq());
+        values.put(User.ADDRESS,user.getAddress());
+        return db.save(TABLENAME,values);
+    }
+}
---
+++ app/src/main/java/com/example/os/myapplication3/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.os.myapplication3;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText nameEditText;
+    private EditText mobileEditText;
+    private EditText qqEditText;
+    private EditText danweiEditText;
+    private EditText addressEditText;
+
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("添加联系人");
+        nameEditText = (EditText)findViewById(R.id.name);
+        mobileEditText = (EditText)findViewById(R.id.mobile);
+        danweiEditText = (EditText)findViewById(R.id.danwei);
+        qqEditText = (EditText)findViewById(R.id.qq);
+        addressEditText = (EditText)findViewById(R.id.address);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        menu.add(Menu.NONE,1,Menu.NONE,"保存");
+        menu.add(Menu.NONE,2,Menu.NONE,"返回");
+        return super.onCreateOptionsMenu(menu);
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case 1:
+                if(!nameEditText.getText().toString().equals("")){
+                User user = new User();
+                    user.setName(nameEditText.getText().toString());
+                    user.setMobile(mobileEditText.getText().toString());
+                    user.setDanwei(danweiEditText.getText().toString());
+                    user.setQq(qqEditText.getText().toString());
+                    user.setAddress(addressEditText.getText().toString());
+
+                    ContactsTable ct = new ContactsTable(MainActivity.this);
+                    if(ct.addData(user)){
+                        Toast.makeText(MainActivity.this,"添加成功",Toast.LENGTH_SHORT).show();
+                        finish();
+                    }else{
+                        Toast.makeText(MainActivity.this,"添加失败",Toast.LENGTH_SHORT).show();
+                    }
+            }else{
+                    Toast.makeText(MainActivity.this,"请先输入数据",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
